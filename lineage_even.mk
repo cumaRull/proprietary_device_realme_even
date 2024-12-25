bdcb047d98635937aa3e6ba0abd732044cfff6ee
CumaRull <102359107+cumaRull@users.noreply.github.com>
2024-12-25 14:49:11 +0700

    even: changes fingerprint

--- lineage_even.mk
+++ lineage_even.mk
@@ -34,15 +34,12 @@ PRODUCT_BRAND := realme
 PRODUCT_MODEL := Realme C25/C25s
 PRODUCT_MANUFACTURER := realme
 
+PRODUCT_GMS_CLIENTID_BASE := android-realme
+
 # Build info
-BUILD_FINGERPRINT := "realme/even/even:11/RP1A.200720.011/1656996364941:user/release-keys"
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_DEVICE=even \
     PRODUCT_NAME=even \
-    PRIVATE_BUILD_DESC="sys_oplus_mssi_64_cn-user 11 RP1A.200720.011 1609743540423 release-keys"
+    PRIVATE_BUILD_DESC="sys_mssi_64_cn_armv82-user 12 SP1A.210812.016 1652065281981 release-keys"
 
-PRODUCT_GMS_CLIENTID_BASE := android-realme
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.build.fingerprint=$(BUILD_FINGERPRINT)
-
+BUILD_FINGERPRINT := realme/RMX3191T2/RMX3191:12/SP1A.210812.016/R.202205091101:user/release-keys
